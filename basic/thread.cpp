69e5559c37dc104651aabb077236e58540b14eb5
Suneet Shukla <suneet.shukla@rapyuta-robotics.com>
2021-01-13 23:46:52 +0900

    Today's basic update

--- basic/thread.cpp
+++ basic/thread.cpp
@@ -2,10 +2,14 @@
 
 
 void hello(){
-    std::cout<<"Hey Everyone"<<std::endl;
+    int num;
+    std::cout<<"Hey! What is your favorite number: "<<std::endl;
+    std::cin>>num;
+    std::cout<<"Interesting! "<<num<<" is my favorite number too, lol. \n";
 }
 
 //int main(){
 //    std::thread t{hello};
 //    return 0;
 //}
+
